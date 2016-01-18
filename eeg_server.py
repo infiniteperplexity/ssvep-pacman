b140d2ab796cb41d850ac6729a5276f5065ef0a2
infiniteperplexity <m543015@CNHARAM3584750.cs.msds.kp.org>
2016-01-18 11:46:28 -0800

    got a simple server set up

---
+++ eeg_server.py
@@ -0,0 +1,42 @@
+message = "The quick brown fox jumps over the lazy dog."
+message = message.split()
+cursor = 0
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        global message
+        global cursor
+        self._set_headers()
+        self.wfile.write('{"'+message[cursor]+'"}')
+        cursor = (cursor+1)%(len(message))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+
+def run(server_class=HTTPServer, handler_class=S, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
